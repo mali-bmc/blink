50475f6220018921237fc5a46a548612d09cea67
mali-bmc <mahmoud_ali@bmc.com>
2016-09-25 21:23:32 -0400

    Update var.sh

--- var.sh
+++ var.sh
@@ -6,11 +6,11 @@ echo "first line"
 ## Don't kickoff a Teamcity build if a succesful one has already ran against the PR.
 PULL_REQ_ID=$1
 
-PR_SHA=$(curl -s -u mali-bmc:Gridapp123 https://api.github.com/repos/Conductor/conductor/pulls/${PULL_REQ_ID} | jq --raw-output .head.sha)
+PR_SHA=$(curl -s -u mali-bmc:Gridapp123 https://api.github.com/repos/Conductor/conductor/pulls/${PULL_REQ_ID})
 
-PR_STATUS=$(curl -s -u mali-bmc:Gridapp123  https://api.github.com/repos/Conductor/conductor/statuses/${PR_SHA} | jq --raw-output  '.[0].state')
+PR_STATUS=$(curl -s -u mali-bmc:Gridapp123  https://api.github.com/repos/Conductor/conductor/statuses/${PR_SHA})
 if [[ ${PR_STATUS} == "success" ]]; then
-	  TC_SUCCESSFUL_BUILD=$(curl -s -u mali-bmc:Gridapp123 https://api.github.com/repos/Conductor/conductor/statuses/${PR_SHA} | jq --raw-output  '.[0].description')
+	  TC_SUCCESSFUL_BUILD=$(curl -s -u mali-bmc:Gridapp123 https://api.github.com/repos/Conductor/conductor/statuses/${PR_SHA})
 	  echo "Teamcity has already reported the success status for the following build: ${TC_SUCCESSFUL_BUILD}"
 
 #TC_SUCCESS_BUILD=$(curl -s -u ${GITHUB_TOKEN}:x-oauth-basic https://api.github.com/repos/Conductor/conductor/statuses/$i | jq --raw-output '.[] | select(.state=="success") | .description')
