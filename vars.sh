37776ee3d55f79d0678107d3299c99b5c45362a7
mali-bmc <mahmoud_ali@bmc.com>
2016-09-26 10:00:05 -0400

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -19,3 +19,6 @@ echo "The branch is $BRANCH";
 GITHUB_TOKEN="c04f75a2016bd1d8f11da49a1840ff1c2582bdf9";
 
 curl -s -u ${GITHUB_TOKEN}:x-oauth-basic https://api.github.com/repos/mali-bmc/blink/commits/${GIT_HASH}/statuses
+
+
+## test the refs/head/*
